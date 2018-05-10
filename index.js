9179263d6d5393c8a969596c4090da5f2112e28b
harrison8024 <harrison8024@gmail.com>
2018-05-10 15:27:27 -0700

    npm script setup

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const PORT = process.env.PORT || 9000;
 
 const app = express();
 
+app.use(cors());
+
+app.get('/user-data', (req,res) => {
+    const user ={
+        name: 'Harrison Chen',
+        email: 'hbc@mail.com',
+    }
+    res.send(user);
+});
+
 app.get('/', (req,res) => {
-    res.send('<h1>App is working!</h1>')
+    res.send('<h1>BackEnd Rocks!</h1>')
 });
 
 app.listen(PORT, ()=>{
