6573b7c9cc18593433d4e5d70d582b86e200727a
harrison8024 <harrison8024@gmail.com>
2018-05-10 17:45:36 -0700

    setup for deployment

--- client/src/components/test.js
+++ client/src/components/test.js
@@ -4,9 +4,25 @@ import axios from 'axios';
 class Test extends Component {
     componentDidMount(){
         this.getUserData();
+        this.getArticle();
+        this.sendData();
+    }
+    async sendData(){
+        const dataToSend = {
+            something:'Here is something',
+            anotherthing: 'Here is another thing',
+            stuff: 'making it up'
+        };
+        const response = await axios.post('/api/send-data',dataToSend);
+
+        console.log('Send Data:', response);
+    }
+    async getArticle(){
+        const response = await axios.get('/api/get-article');
+        console.log('Article data:', response);
     }
     async getUserData(){
-        const response = await axios.get('http://localhost:9000/user-data');
+        const response = await axios.get('/api/user-data');
         console.log('User Data Response', response);
     }
     render(){
--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+const {resolve} = require('path');
 const PORT = process.env.PORT || 9000;
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.static(resolve(__dirname, 'client', 'dist')));
 
-app.get('/user-data', (req,res) => {
+app.post('/api/send-data', (req,res)=>{
+    console.log('Data sent:', req.body);
+    res.send({success:true, mirror: req.body});
+});
+
+app.get('/api/user-data', (req,res) => {
     const user ={
         name: 'Harrison Chen',
         email: 'hbc@mail.com',
-    }
+    };
     res.send(user);
 });
 
-app.get('/', (req,res) => {
-    res.send('<h1>BackEnd Rocks!</h1>')
+app.get('/api/get-article',(req,res)=>{
+    const article ={
+        title:'Some article',
+        author:'John Walker',
+        content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, unde.'
+    };
+    res.send(article);
+});
+app.get('*', (req,res)=>{
+    res.sendFile(resolve(__dirname, 'client', 'dist', 'index.html'));
 });
 
 app.listen(PORT, ()=>{
